f5f68d000235f328e7a181c2ea89907c53e5e462
djmarcy <dj_marcy@hotmail.com>
2022-03-18 18:41:39 -0600

    post-tutoring push w/ next steps

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let api = require("./routes/index");
+let fs = require('fs');
+let path = require('path')
 
 let PORT = process.env.PORT || 3001;
 
@@ -8,11 +10,27 @@ let app = express();
 //Middleware in use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
+app.use(express.static('public'))
+// app.use("/api", api);
+
+// Inter
+app.get("/api/notes", (req, res) => {
+  // For each of the 3 routes, I'll need to load separately each time; function to handle?
+  // For DELETE, read notes, filter by specified note, delete specified note, save others back to db.json file
+  // Assign each note a unqiue ID key
+    const notes = JSON.parse(fs.readFileSync("db/db.json"))
+    
+    res.json(notes);
+  }
+);
 
 // Direct traffic to homepage
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 // Wildcard route to redirect to homepage
@@ -21,3 +39,6 @@ app.get("*", (req, res) =>
 );
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
+
+
+// Next teps
