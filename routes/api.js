c43227c345fde21fbcb54cf27c542975c01dba35
djmarcy <dj_marcy@hotmail.com>
2022-03-22 18:44:35 -0600

    everything but delete works like a charm

--- routes/api.js
+++ routes/api.js
@@ -1,48 +1,70 @@
 // Required modules
+const { json } = require("express/lib/response");
 let fs = require("fs");
 let router = require("express").Router();
 let path = require("path");
 let id = require("uniqid");
 
-// Path to stored notes
-let notesAPI = require("../db/db.json");
-
-// What do I need to do next?
-
 // 1. GET current notes (retrieve JSON file, parse it, display on page)
-router.get("/notes", (req, res) =>
-  fs.readFile("../db/db.json").then((data) => {
-    return res.json(JSON.parse(data));
-  })
+router.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+
+    if (err) throw err
+
+    return res.json(JSON.parse(data))
+  })}
 );
 
 // 2. POST new note to JSON file (stringify, store, reload current nodes w/ new note front-and-center)
 
 router.post("/notes", (req, res) => {
-  // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
 
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
-    const newTask = {
-      title,
-      text,
-      taskID: uuidv4(),
-    };
+  let oldData = require('../db/db.json')
 
-    readAndAppend(newTask, "../db/db.json");
+  const { title, text } = req.body
 
-    const response = {
-      status: "success",
-      body: newTask,
-    };
-
-    res.json(response);
-  } else {
-    res.json("Error in posting feedback");
+  let newTip = {
+    title: title,
+    text: text,
+    id: id()
   }
+
+  oldData.unshift(newTip)
+
+  console.log(oldData)
+
+  let newData = JSON.stringify(oldData)
+
+  console.log(newData)
+
+  // Take db.json
+  fs.writeFile("./db/db.json", newData, (err) => {
+    if (err) throw err
+
+    console.log("Tip successfully posted!")
+    router.get("/notes", (req, res) => {
+      fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+    
+        if (err) throw err
+    
+        return res.json(JSON.parse(data))
+      })}
+    );
+  })
+
+  res.end();
 });
+
+router.delete("/notes/:id", (res, req) => {
+
+  let oldData = require('../db/db.json');
+
+  let toDelete = oldData
+
+  console.log(toDelete)
+
+})
+
 
 // 3. Assign unique ID to each note in JSON file so you can delete specific notes
 
--- server.js
+++ server.js
@@ -15,17 +15,7 @@ app.use(express.static("public"));
 
 // Direct traffic
 app.use("/", index);
-app.use("/notes", notesAPI);
-
-// Inter
-// app.get("/notes", (req, res) => {
-//   // For each of the 3 routes, I'll need to load separately each time; function to handle?
-//   // For DELETE, read notes, filter by specified note, delete specified note, save others back to db.json file
-//   // Assign each note a unqiue ID key
-//   const notes = JSON.parse(fs.readFileSync("db/db.json"));
-
-//   res.json(notes);
-// });
+app.use("/api", notesAPI);
 
 app.listen(PORT, () =>
   console.log(`Listening at port http://localhost:${PORT}`)
